dd18c99c7d51afcd4fe7a7ba16c9e36e072e3696
mnmariano <mn.invalidtarget@gmail.com>
2023-11-29 11:58:24 -0300

    Subida inicial

---
+++ src/test/java/com/cac23650/TP/TpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cac23650.TP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
